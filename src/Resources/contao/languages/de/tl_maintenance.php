69d295cb95696df324c0dda01bd008b6019d93c1
Peter Adelmann <peter.adelmann@trilobit.de>
2022-03-14 20:40:13 +0000

    Feature/initial commit

---
+++ src/Resources/contao/languages/de/tl_maintenance.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  trilobit GmbH
+ * @author     trilobit GmbH <https://github.com/trilobit-gmbh>
+ * @license    LGPL-3.0-or-later
+ * @link       http://github.com/trilobit-gmbh/contao-refresh-bundle
+ */
+
+$GLOBALS['TL_LANG']['tl_maintenance']['refreshtarget']['headline'] = 'Refresh';
+$GLOBALS['TL_LANG']['tl_maintenance']['refreshtarget']['description'] = 'Hier können Sie die Inhalte des ausgewählten Zielsystems aktualisieren. Es wird der Contao-Manager benötigt (<a href="https://contao.org/de/download.html"><u>https://contao.org/de/download.html</u></a>).';
+
+$GLOBALS['TL_LANG']['tl_maintenance']['refreshtarget']['maintenance'] = 'Der Refresh ist im Wartungsmodus nicht verfügbar.';
+$GLOBALS['TL_LANG']['tl_maintenance']['refreshtarget']['active'] = 'Es wird bereist ein Refresh-Prozess ausgeführt. Bitte versuchen Sie es später noch einmal.';
+
+$GLOBALS['TL_LANG']['tl_maintenance']['refreshtarget']['error'] = 'Es wurde kein Task definiert.';
+
+$GLOBALS['TL_LANG']['tl_maintenance']['refreshtarget']['submit'] = 'Refresh starten';
+$GLOBALS['TL_LANG']['tl_maintenance']['refreshtarget']['confirm'] = 'Soll der Refresh wirklich ausgeführt werden?';
+$GLOBALS['TL_LANG']['tl_maintenance']['refreshtarget']['done'] = 'Refresh abgeschlossen';
+
+$GLOBALS['TL_LANG']['tl_maintenance']['refreshtarget']['environments'][0] = 'Task';
+$GLOBALS['TL_LANG']['tl_maintenance']['refreshtarget']['environments'][1] = 'Bitte wählen Sie einen Refresh-Task aus.';
